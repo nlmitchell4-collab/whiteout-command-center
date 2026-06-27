7e9e8401bb0e3221412fce1ddf634d8148925b65
Nick Mitchell <nlmitchell4@gmail.com>
2026-06-27 13:45:40 -0500

    refactor: complete scss migration and restore responsiveness

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import "../scss/main.scss";
+
+import "./navigation.js";
+import "./chiefs.js";
+import "./assignments.js";
+import "./battlefield.js";
+import "./summary.js";
+import "./myObjectives.js";
+import "./app.js";
