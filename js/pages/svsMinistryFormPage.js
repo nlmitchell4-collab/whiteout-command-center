209b528dc837353ee826a483ba651db0b96ac74a
Nick Mitchell <nlmitchell4@gmail.com>
2026-08-06 20:15:58 -0500

    Fix SVS ministry form embed deployment

--- js/pages/svsMinistryFormPage.js
+++ js/pages/svsMinistryFormPage.js
@@ -1,5 +1,5 @@
 const SVS_FORM_WEB_APP_URL =
-    "https://script.google.com/macros/s/AKfycbwtWQfbWoZWSs3MVcy5wAM0EyxSVOfTclM3HTfD2qw/exec";
+    "https://script.google.com/macros/s/AKfycbyYRZRUyw33cG3HKxINpUHCNPkaDh6EQlaHW_XydJZ0NnEklQ5P3eg1ISQ3M5uG_DY-/exec";
 
 export function renderSvsMinistryFormPage() {
     const container =
